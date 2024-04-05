6e569a34fa9e8234ae052473dc573b5ebc7cfa74
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-06 04:05:54 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function replaceJavascriptiwithTypescript(sentence) {
+    return sentence.replace(/JavaScript/g, "TYPE SCRIPT");
+}
+console.log(replaceJavascriptiwithTypescript("I love JavaScript and to learn JavaScript is quite fun to learn"));
---
+++ index.ts
@@ -0,0 +1,5 @@
+function replaceJavascriptiwithTypescript(sentence:string):string{
+    return sentence.replace(/JavaScript/g, "TYPE SCRIPT")
+}
+console.log(replaceJavascriptiwithTypescript
+    ("I love JavaScript and to learn JavaScript is quite fun to learn"))
